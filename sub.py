f2ac1908e9758b85697cb989fb17c38d264eb529
Tarjei Utnes <tutnes@users.noreply.github.com>
2016-04-18 00:42:45 +0200

    Delete sub.py

--- sub.py
+++
@@ -1,5 +0,0 @@
-from gcloud import pubsub
-client = pubsub.Client(        project='practical-brace-126614')
-topic = client.topic('topic_name')
-subscription = topic.subscription('subscription_name')
-subscription.create()  # API request
